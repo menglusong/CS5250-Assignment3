6905faa5baea756be3ca88a7c71bed5534d2bd21
songmenglu <songmenglu@gmail.com>
2017-04-08 18:58:46 +0800

    change onebyte_data to four_MB_data, and rewrite onebyte_read() and onebyte_write() to extend /dev/one to 4MB

--- one.c
+++ one.c
@@ -8,6 +8,7 @@
 #include <linux/proc_fs.h>
 #include <asm/uaccess.h>
 #define MAJOR_NUMBER 61
+#define DEVICE_SIZE 4000000
  
 /* forward declaration */
 int onebyte_open(struct inode *inode, struct file *filep);
@@ -23,7 +24,7 @@ struct file_operations onebyte_fops = {
      open:     onebyte_open,
      release: onebyte_release
 };
-char *onebyte_data = NULL;
+char *four_MB_data = NULL;
 
 int onebyte_open(struct inode *inode, struct file *filep)
 {
@@ -37,24 +38,48 @@ int onebyte_release(struct inode *inode, struct file *filep)
 
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {
-     copy_to_user(buf, onebyte_data, 1);
-     if (*f_pos == 0) {
-          *f_pos+=1;
-          return 1;
-     } else {
-          return 0;
-     }
+    int maxbytes; /*maximum bytes that can be read from f_pos to DEVICE_SIZE*/
+    int bytes_to_read; /* gives the number of bytes to read*/
+    int bytes_read; /*number of bytes actually read*/
+
+    maxbytes = DEVICE_SIZE - *f_pos;
+    if (maxbytes > count)
+        bytes_to_read = count;
+    else
+        bytes_to_read = maxbytes;
+
+    if (bytes_to_read == 0) {
+        printk(KERN_INFO "Reached the end of the device\n");
+        return 0;
+    }
+    bytes_read = bytes_to_read - copy_to_user(buf, four_MB_data + *f_pos, bytes_to_read);
+    printk(KERN_INFO "%d Byte(s) has been read \n", bytes_read);
+    *f_pos += bytes_read;
+    return bytes_read;
 }
 
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
-      if (count) {
-          copy_from_user(onebyte_data, buf, 1);
+      int maxbytes; /*maximum bytes that can be read from ppos to DEVICE_SIZE*/
+      int bytes_to_write; /* gives the number of bytes to write*/
+      int bytes_writen; /*number of bytes actually writen*/
+      maxbytes = DEVICE_SIZE - *f_pos;
+      if (maxbytes > count)
+          bytes_to_write = count;
+      else
+          bytes_to_write = maxbytes;
+
+      if (count && bytes_to_write) {
+          bytes_writen = bytes_to_write - copy_from_user(four_MB_data + *f_pos, buf, bytes_to_write);
+          printk(KERN_INFO "%d has been written to device\n", bytes_writen);
+          *f_pos += bytes_writen;
+          printk(KERN_INFO "Done writing to device\n");
+          return bytes_writen;
       }
-      if (count>1) {
+
+      if (count > DEVICE_SIZE) {
           return -ENOSPC;
       }
-      return 1;
 }
 
 static int onebyte_init(void)
@@ -69,31 +94,30 @@ static int onebyte_init(void)
      // kmalloc is just like malloc, the second parameter is
      // the type of memory to be allocated.
      // To release the memory allocated by kmalloc, use kfree.
-     onebyte_data = kmalloc(sizeof(char), GFP_KERNEL);
-     
-     if (!onebyte_data) {
+     four_MB_data = kmalloc(DEVICE_SIZE, GFP_KERNEL);
+     if (!four_MB_data) {
           onebyte_exit();
           // cannot allocate memory
           // return no memory error, negative signify a failure
           return -ENOMEM;
      }
      // initialize the value to be X
-     *onebyte_data = 'X';
-     printk(KERN_ALERT "This is a onebyte device module\n");
+     *four_MB_data = 'X';
+     printk(KERN_ALERT "This is a 4 MB device module\n");
      return 0;
 }
 
 static void onebyte_exit(void)
 {
      // if the pointer is pointing to something
-     if (onebyte_data) {
+     if (four_MB_data) {
           // free the memory and assign the pointer to NULL
-          kfree(onebyte_data);
-          onebyte_data = NULL;
+          kfree(four_MB_data);
+          four_MB_data = NULL;
      }
      // unregister the device
      unregister_chrdev(MAJOR_NUMBER, "onebyte");
-     printk(KERN_ALERT "Onebyte device module is unloaded\n");
+     printk(KERN_ALERT "4 MB device module is unloaded\n");
 }
 
 MODULE_LICENSE("GPL");
